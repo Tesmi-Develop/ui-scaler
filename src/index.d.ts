2fcd65310e0efe497abf63b3729b4cc931881f41
Tesmi-Devlop <sashdfr@gmail.com>
2024-05-30 18:30:48 +1000

    Added some hooks

---
+++ src/index.d.ts
@@ -0,0 +1,19 @@
+import { Binding } from "@rbxts/react";
+
+declare namespace UIScaler {
+	export function useScaler(baseResolution: Vector2): ScalerApi;
+
+	function usePx(px: number): usePxApi;
+
+	export interface ScalerApi {
+		scale: Binding<number>;
+		usePx: typeof usePx;
+	}
+
+	export interface usePxApi {
+		(value: number): number;
+	}
+}
+
+export = UIScaler;
+export as namespace Charm;
